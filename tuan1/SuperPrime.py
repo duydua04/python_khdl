0d72a4e71bca809d5cd81cc870d09cf71e58d0c5
Hoang Dinh Duy <duycp102k3@gmail.com>
2025-09-12 15:46:42 +0700

    add bai2

---
+++ tuan1/SuperPrime.py
@@ -0,0 +1,20 @@
+from math import sqrt
+
+def is_prime(n: int):
+    if n <= 1:
+        return False
+
+    for i in range(2, int(sqrt(n)) +1):
+        if n % i == 0:
+            return False
+
+    return True
+
+def is_super_prime(n: int):
+    if n < 10:
+        return is_prime(n)
+
+    return is_prime(n) and is_prime(n // 10)
+
+n = int(input())
+print(is_super_prime(n))
