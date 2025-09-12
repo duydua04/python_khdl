59ed980edbefdfc3d11250c55d752e2efa8e4f62
Hoang Dinh Duy <duycp102k3@gmail.com>
2025-09-12 17:25:09 +0700

    Bai 8 tuan 1

---
+++ tuan1/palindrome.py
@@ -0,0 +1,3 @@
+def is_palindrome(p):
+    s = str(p)
+    return s == s[::-1]
--- tuan1/sumNN.py
+++ tuan1/sumNN.py
@@ -6,7 +6,5 @@ def nn2(n):
     result = int(n) + int(n*2) + int(n*3) + int(n*4)
     print(result)
 
-n1 = int(input())
 n2 = input()
-nn1(n1)
 nn2(n2)
